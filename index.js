6ab24d86adc088a64643a48b93c02cb83ad2af86
KevinKiseli <kevin.kiseli@student.moringaschool.com>
2023-09-23 14:44:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(Kevin){
+    return `Hi, my name is ${Kevin}.`;
+}
+
+function introductionWithLanguage(Kevin, language){
+    return `Hi, my name is ${Kevin} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(Kevin, language ="JavaScript"){
+    return `Hi, my name is ${Kevin} and I am learning to program in ${language}.`
+}
